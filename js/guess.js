ea1eda161cd602b7ba506affb6c9caa737b5a0c3
tokhi <shafi.tokhi@gmail.com>
2015-01-23 00:25:44 +0100

    number guess - no number of tries

---
+++ js/guess.js
@@ -0,0 +1,49 @@
+angular.module('numGuess', []).constant()
+	//.factory()
+	.controller('NumberGuessController', ['$scope', function ($scope) {
+		$scope.max = 100;
+		
+
+		$scope.getRandomNumber = function () {
+			return Math.floor((Math.random() * $scope.max) + 1);
+		}
+
+		$scope.number = '';
+		$scope.numberOfTries = 0;
+
+		
+		var rNum = $scope.getRandomNumber();
+		$scope.gameOn = true;
+		
+
+		
+
+		$scope.target = function () {
+
+			if($scope.number == rNum) {
+				$scope.gameOn = false;
+				return "Good Job, You are right!";
+			}
+			else {
+				if($scope.number == '')
+					return "No guess yet!";
+				else if($scope.number > rNum)
+					return "Wrong Guess, choose a number < " +$scope.number;
+				else
+					return "Wrong Guess, choose a number > " +$scope.number;
+			}
+			
+		}
+
+		$scope.repeatGame = function () {
+			numberOfTries = 0;
+			$scope.number = '';
+			rNum = $scope.getRandomNumber();
+			$scope.gameOn = true;
+		}
+
+		$scope.tries = function () {
+			return numberOfTries;
+		}
+		
+	}]);
